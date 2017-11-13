fb76dc1342be85e2de8c0135a115bc321488fbbe
yanxu <yan.xu@rokt.com>
2017-11-13 22:22:02 +1100

    lr

--- algTrading/basic_lr.py
+++ algTrading/basic_lr.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 from sklearn import linear_model as lm
 from sklearn.metrics import mean_squared_error
@@ -18,15 +19,22 @@ class LinearRegressionModel:
         self.x_cols_bid = []
         self.y_cols_bid = []
 
+        self.y_pred_cols_bid = []
+        self.y_pred_cols_ask = []
+
         self.train_df = None
         self.test_df = None
 
     def load_data(self):
         self.train_df = pd.read_csv(TRAINING_DATA_PATH)
 
-        for i in range(51, 52):
+        for i in range(52, 53):
             self.y_cols_bid.append("bid{}".format(i))
             self.y_cols_ask.append("ask{}".format(i))
+
+        for i in range(52, 101):
+            self.y_pred_cols_bid.append("bid{}".format(i))
+            self.y_pred_cols_ask.append("ask{}".format(i))
 
         for i in range(1, 51):
             self.x_cols_bid.append("bid{}".format(i))
@@ -57,12 +65,17 @@ class LinearRegressionModel:
         lr_model_bid = lm.LinearRegression()
         lr_model_bid.fit(self.X_bid.todense(), self.y_bid.values)
         estimated_y_bid = lr_model_bid.predict(self.X_bid.todense())
-        print("training RMSE: {}".format(sqrt(mean_squared_error(self.y_bid, estimated_y_bid))))
+
+        bid_true_y = self.train_df[self.y_pred_cols_bid].values.flatten('F')
+        bid_predicted_y = np.repeat(estimated_y_bid, 49)
 
         lr_model_ask = lm.LinearRegression()
         lr_model_ask.fit(self.X_ask.todense(), self.y_ask.values)
         estimated_y_ask = lr_model_bid.predict(self.X_ask.todense())
-        print("training RMSE: {}".format(sqrt(mean_squared_error(self.y_ask.values, estimated_y_ask))))
+
+        ask_true_y = self.train_df[self.y_pred_cols_ask].values.flatten('F')
+        ask_predicted_y = np.repeat(estimated_y_ask, 49)
+        print("training RMSE: {}".format(sqrt(mean_squared_error(np.append(bid_true_y, ask_true_y), np.append(bid_predicted_y, ask_predicted_y)))))
 
     def model_prediction(self):
         pass
